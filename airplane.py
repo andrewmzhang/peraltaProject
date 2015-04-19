146f336c767a6f1480c61cfb492dff221098dc5b
malus <andrewmzhang12345@gmail.com>
2015-04-19 13:10:32 -0700

    added python code

---
+++ airplane.py
@@ -0,0 +1,61 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(7, GPIO.IN)
+GPIO.setup(8, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(18, GPIO.IN)
+
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+int detection() {
+	inputPins = [4, 7, 8, 17, 18]
+	for x in inputPins:
+		if GPIO.input(x) == true:
+			return x
+		else:
+			return 
+}
+
+void shutdownAllNodes(){
+	outputs = [21, 22, 23, 24, 25]
+	for x in outputs:
+		GPIO.output(x, False)
+}
+
+
+void decisionTree(int x) {
+	shutdownAllNodes()
+	if x == 4:
+		GPIO.output(21, True)
+	if x == 7:
+		GPIO.output(22, True)
+		GPIO.output(21, True)
+	if x == 8:
+		GPIO.output(23, True)
+		GPIO.output(22, True)
+		GPIO.output(21, True)
+	if x == 17:
+		GPIO.output(24, True)
+		GPIO.output(23, True)
+		GPIO.output(22, True)
+		GPIO.output(21, True)
+	if x == 18:
+		GPIO.output(25, True)
+		GPIO.output(24, True)
+		GPIO.output(23, True)
+		GPIO.output(22, True)
+		GPIO.output(21, True)
+		playSound()
+}
+
+void playSound() {
+
+}
+
